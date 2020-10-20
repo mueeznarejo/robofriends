bf8cc2c537afeaa614f6db9f61de058af9882b01
mueeznarejo <mueez.dev@gmail.com>
2020-10-20 17:57:18 +0500

    Updates

---
+++ precache-manifest.8d7c078c14410ea9d65ba0cee950be80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a26a8c96cc32c919639d8babc2bad0c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5578757ce4a771a9201b",
+    "url": "/robofriends/static/css/main.6956a4de.chunk.css"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/robofriends/static/js/2.14bc996e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.14bc996e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5578757ce4a771a9201b",
+    "url": "/robofriends/static/js/main.2b7ee789.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
